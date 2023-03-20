82cfa255200c83d4178aef700c2702e6a80f8732
vignesh0311 <vigneshmmv0311@gmail.com>
2023-03-20 16:58:54 +0530

    createVignesh component

---
+++ src/app/vignesh/vignesh.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vignesh',
+  templateUrl: './vignesh.component.html',
+  styleUrls: ['./vignesh.component.scss']
+})
+export class VigneshComponent {
+
+  name:any;
+
+}
