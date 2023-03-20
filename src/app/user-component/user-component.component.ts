5d48ec8a7b3454f8570c801049e731761410fad8
Oliver2701 <abbrittooliver1999@gmail.com>
2023-03-20 17:02:22 +0530

    create oliver component

---
+++ src/app/user-component/user-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-component',
+  templateUrl: './user-component.component.html',
+  styleUrls: ['./user-component.component.scss']
+})
+export class UserComponentComponent {
+
+  title = "OLIVER";
+}
