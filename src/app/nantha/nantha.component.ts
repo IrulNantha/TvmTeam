f408b626f3625187cbb041d9b8419c01a4083665
IrulNantha <irulnantha374@gmail.com>
2023-03-20 16:56:54 +0530

    createNantha comonent

---
+++ src/app/nantha/nantha.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nantha',
+  templateUrl: './nantha.component.html',
+  styleUrls: ['./nantha.component.scss']
+})
+export class NanthaComponent {
+
+}
